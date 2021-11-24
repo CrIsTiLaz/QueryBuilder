d9b549d64dae8a94315f95dca38c7d7c7e084c41
CrIsTiLaz <cristilazea18@gmail.com>
2021-11-24 21:45:49 +0200

    with logg

---
+++ QueryBuilder/Models/Logger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Web;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace QueryBuilder.Models.Log
+{
+        public abstract class LogBase
+        {
+            public abstract void Log(string Messsage);
+        }
+
+        public class Logger : LogBase
+        {
+
+            private String CurrentDirectory
+            {
+                get;
+                set;
+            }
+
+            private String FileName
+            {
+                get;
+                set;
+            }
+
+            private String FilePath
+            {
+                get;
+                set;
+            }
+
+            public Logger()
+            {
+                this.CurrentDirectory = Thread.GetDomain().BaseDirectory;
+                this.FileName = "Log.txt";
+                this.FilePath = this.CurrentDirectory + this.FileName;
+
+            }
+
+            public override void Log(string Messsage)
+            {
+
+                System.Console.WriteLine("Logged : {0}", Messsage);
+
+                using (System.IO.StreamWriter w = File.AppendText(this.FilePath))
+                {
+                    w.Write("\r\nLog Entry : ");
+                    w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                    w.WriteLine("  :{0}", Messsage);
+                    w.WriteLine("-----------------------------------------------");
+                }
+            }
+        }
+    }
